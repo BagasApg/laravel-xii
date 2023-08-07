fea57db9b0c2209e971822b175efb9408a1b73f6
BagasApg <bagasapg@gmail.com>
2023-08-07 17:03:28 +0700

    added contacts(edit left), model lesson mapel

--- resources/views/contact/contact.blade.php
+++ resources/views/contact/contact.blade.php
@@ -6,18 +6,57 @@
     <div class="card">
         <div class="card-header">
             <h3>Contacts</h3>
+
+            <a href="/contact/tambah">
+                <div class="btn btn-success">Tambah</div>
+            </a>
+            <a href="/contact/edit">
+                <div class="btn btn-primary ms-2">Edit</div>
+            </a>
+
         </div>
-        <div class="card-body">
+        <div class="card-body d-flex flex-column gap-3">
 
-            <div class="btn btn-primary">
+            <div class="card btn contact-cards">
+                <div class="btn card-body text-start p-3">
+                    <a href="http://instagram.com">
+                        <div class="d-flex flex-row gap-3 align-items-center">
 
-                <div class="d-flex flex-row gap-3 align-items-center">
+                            <i style="width: 32px; height: 32px" data-feather="instagram"></i>
+                            <div class="col">
+                                <p class="m-0">Bagas Arianto</p>
+                                <p class="m-0 form-text">Instagram</p>
+                            </div>
+                        </div>
+                    </a>
+                </div>
+            </div>
+            <div class="card btn contact-cards">
+                <div class="btn card-body text-start p-3">
+                    <a href="http://instagram.com">
+                        <div class="d-flex flex-row gap-3 align-items-center">
 
-                    <i style="width: 32px; height: 32px" data-feather="instagram"></i>
-                    <div class="col">
-                        <p class="m-0">Bagas Arianto</p>
-                        <p class="m-0 form-text">Instagram</p>
-                    </div>
+                            <i style="width: 32px; height: 32px" data-feather="youtube"></i>
+                            <div class="col">
+                                <p class="m-0">Reddssss</p>
+                                <p class="m-0 form-text">Youtube</p>
+                            </div>
+                        </div>
+                    </a>
+                </div>
+            </div>
+            <div class="card btn contact-cards">
+                <div class="btn card-body text-start p-3">
+                    <a href="http://twitch.com">
+                        <div class="d-flex flex-row gap-3 align-items-center">
+
+                            <i style="width: 32px; height: 32px" data-feather="twitch"></i>
+                            <div class="col">
+                                <p class="m-0">Redddsss</p>
+                                <p class="m-0 form-text">Twitch</p>
+                            </div>
+                        </div>
+                    </a>
                 </div>
             </div>
         </div>
--- resources/views/contact/contacttambah.blade.php
+++ resources/views/contact/contacttambah.blade.php
@@ -1,7 +1,61 @@
 @extends('templates.index')
 
-@section('title','Contact')
+@section('title', 'Contact')
 
 @section('content')
-<h1>tambah kuntals</h1>
+    <div class="m-1 mb-3">
+        <div class="d-flex align-items-center">
+            <a href="/contact" class="text-decoration-none text-black ">
+                <div class="d-flex align-items-center back">
+
+
+                    <i class="back-icon" data-feather="chevron-left"></i>
+                    <p class="m-0 fs-5">Back</p>
+                </div>
+            </a>
+        </div>
+
+    </div>
+    <div class="card">
+        <div class="m-4">
+            <h4>Tambah Kontak</h4>
+            <div class="row">
+                <div class="col-md-6">
+                    <div class="">
+                        <label for="defaultFormControlInput" class="form-label">Username</label>
+                        <input type="text" class="form-control" id="defaultFormControlInput" placeholder="Bagas Arianto"
+                            aria-describedby="defaultFormControlHelp" />
+                        <div id="defaultFormControlHelp" class="form-text">
+                        </div>
+
+                    </div>
+
+                </div>
+                <div class="col-md">
+                    <div>
+                        <label for="defaultFormControlInput" class="form-label">Email</label>
+                        <input type="email" class="form-control" id="defaultFormControlInput"
+                            placeholder="bagas.arianto@eveger.com" aria-describedby="defaultFormControlHelp" />
+                        <div id="defaultFormControlHelp" class="form-text">
+                        </div>
+                    </div>
+
+                </div>
+
+            </div>
+            <div class="row">
+                <div>
+                    <label for="defaultFormControlInput" class="form-label">Link</label>
+                    <input type="email" class="form-control" id="defaultFormControlInput" placeholder="https://instagram.com"
+                        aria-describedby="defaultFormControlHelp" />
+                    <div id="defaultFormControlHelp" class="form-text">
+                    </div>
+                </div>
+            </div>
+            <div class="mt-3 add-button">
+                <div class="btn btn-success">Tambah</div>
+            </div>
+        </div>
+
+    </div>
 @endsection
